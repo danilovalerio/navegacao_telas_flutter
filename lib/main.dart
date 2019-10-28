0307866e2cd0ac6470faf439e71b6f539a0f13d7
dvalerio <dnotms290>
2019-10-28 19:13:37 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Ir para segunda tela"),
+              padding: EdgeInsets.all(15),
+              onPressed: (){
+                Navigator.push(
+                    context,
+                    route);
+              },
+
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+
+}
