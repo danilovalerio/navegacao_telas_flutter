6383b51d14b7d0ef747d8a01cdc62041450c85b5
dvalerio <dnotms290>
2019-10-30 10:59:16 -0300

    Passagem de parâmetro entre a navegação

--- lib/TelaSecundaria.dart
+++ lib/TelaSecundaria.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class TelaSecundaria extends StatefulWidget {
+  //para uso no construtor
+  String valor;
+
+//  TelaSecundaria(this.valor);
+  //com chaves se torna opcional a passagem de parâmetro
+  TelaSecundaria({this.valor});
+
   @override
   _TelaSecundariaState createState() => _TelaSecundariaState();
 }
 
 class _TelaSecundariaState extends State<TelaSecundaria>{
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +27,7 @@ class _TelaSecundariaState extends State<TelaSecundaria>{
         padding: EdgeInsets.all(32),
         child: Column(
           children: <Widget>[
-            Text("Segunda tela")
+            Text("Segunda tela! Valor passado: ${widget.valor}")
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:telas_navegacao_flutter_app/TelaSecundaria.dart';
 void main(){
   runApp(MaterialApp(
     home: TelaPrincipal(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -32,7 +33,7 @@ class _TelaPrincipalState extends State<TelaPrincipal>{
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => TelaSecundaria()
+                      builder: (context) => TelaSecundaria(valor: "Valor da tela 1")
                     ));
               },
 
